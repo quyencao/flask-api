9c8d583b8b930fdb63c9b5b6c0681bd2fd645358
Quyền Cao <boy20195@gmail.com>
2018-06-18 11:53:12 +0700

    Update run.py

--- run.py
+++ run.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     
 # http://www.math-cs.gordon.edu/courses/cs211/ATMExample/
 # http://buildingskills.itmaybeahack.com/book/oodesign-3.1/html/index.html
+# http://massivetechinterview.blogspot.com/2015/07/design-chess-game-using-oo-principles.html
