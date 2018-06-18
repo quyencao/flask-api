ce53de4da5d47c1c8f0587fa742514cce6a7d815
Quyền Cao <boy20195@gmail.com>
2018-06-18 11:40:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,3 +2,6 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=app.config.get('DEBUG'))
+    
+# http://www.math-cs.gordon.edu/courses/cs211/ATMExample/
+# http://buildingskills.itmaybeahack.com/book/oodesign-3.1/html/index.html
